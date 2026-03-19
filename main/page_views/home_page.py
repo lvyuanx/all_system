4c2df878c127b77df301bb68bb615f061f7ebafd
lvyuanxiang <2466057319@qq.com>
2026-03-19 11:33:52 +0800

    feat: 实现用户首页和超级管理员数据看板

--- main/page_views/home_page.py
+++ main/page_views/home_page.py
@@ -1,8 +1,27 @@
+import json
 from django.shortcuts import render
 
 
 def home_page(request):
-    context = {
-        "title": "订单创建",
-    }
-    return render(request, "custom_home/index.html", context)
+    user = request.user
+    if user.is_superuser:
+        context = {"title": "数据看板"}
+        template = "custom_home/superuser_index.html"
+    else:
+        role_names = list(user.groups.values_list("name", flat=True))
+        user_info = {
+            "username": user.username,
+            "name": user.get_full_name() or user.username,
+            "email": user.email,
+            "last_login": user.last_login and user.last_login.strftime("%Y-%m-%d %H:%M:%S"),
+            "is_staff": user.is_staff,
+            "roles": role_names,
+        }
+
+        context = {
+            "title": "欢迎",
+            "user_info_json": json.dumps(user_info, ensure_ascii=False),
+        }
+        template = "custom_home/user_index.html"
+    return render(request, template, context)
+
